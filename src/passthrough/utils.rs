a49914ec6fd002b6e764c4ec94fc50004bd71eb0
martyncharler <martyn.harler@me.com>
2023-12-30 10:57:59 +0100

    tests moved into test block

--- src/passthrough/utils.rs
+++ src/passthrough/utils.rs
@@ -3,11 +3,6 @@ extern crate pnet;
 use pnet::datalink::{NetworkInterface};
 use socket2::{Domain, SockAddr, Socket, Type};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::ipv4::MutableIpv4Packet;
-use pnet::packet::tcp;
-use pnet::packet::tcp::MutableTcpPacket;
-use pnet_macros_support::packet::Packet;
 
 // health ports are reserved for health checks
 pub const HEALTH_PORT_LOWER: u16 = 32768;
@@ -20,63 +15,79 @@ pub const TCP_HEADER_LEN: usize = 32;
 // leave for reference
 // pub const ETHERNET_HEADER_LEN: usize = 14;
 
+
 #[cfg(test)]
-pub fn find_local_addr() -> Option<Ipv4Addr> {
-    for iface in pnet::datalink::interfaces() {
-        for ipnet in iface.ips {
-            if ipnet.is_ipv4() {
-                match ipnet.ip() {
-                    IpAddr::V4(ip) => return Some(ip),
-                    _ => continue,
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr};
+    use pnet::packet::ip::IpNextHeaderProtocols;
+    use pnet::packet::ipv4::MutableIpv4Packet;
+    use pnet::packet::tcp;
+    use pnet::packet::tcp::MutableTcpPacket;
+    use pnet_macros_support::packet::Packet;
+    use crate::passthrough::utils::{IPV4_HEADER_LEN, TCP_HEADER_LEN};
+
+    #[test]
+    pub fn find_local_addr() -> Option<Ipv4Addr> {
+        for iface in pnet::datalink::interfaces() {
+            for ipnet in iface.ips {
+                if ipnet.is_ipv4() {
+                    match ipnet.ip() {
+                        IpAddr::V4(ip) => return Some(ip),
+                        _ => continue,
+                    }
                 }
             }
         }
+        None
     }
-    None
+
+    #[test]
+    pub fn build_dummy_ip(
+        src_ip: Ipv4Addr,
+        dst_ip: Ipv4Addr,
+        src_port: u16,
+        dst_port: u16,
+    ) -> MutableIpv4Packet<'static> {
+        // Setup TCP header
+        let mut vec: Vec<u8> = vec![0; TCP_HEADER_LEN];
+        let mut tcp_header = MutableTcpPacket::new(&mut vec[..]).unwrap();
+
+        tcp_header.set_source(src_port);
+        tcp_header.set_destination(dst_port);
+
+        tcp_header.set_flags(tcp::TcpFlags::SYN);
+        tcp_header.set_window(64240);
+        tcp_header.set_data_offset(8);
+        tcp_header.set_urgent_ptr(0);
+        tcp_header.set_sequence(rand::random::<u32>());
+
+        let checksum = pnet::packet::tcp::ipv4_checksum(&tcp_header.to_immutable(), &src_ip, &dst_ip);
+        tcp_header.set_checksum(checksum);
+
+        // Setup IP header
+        let ipbuf: Vec<u8> = vec![0; TCP_HEADER_LEN + IPV4_HEADER_LEN];
+        let mut ip_header = MutableIpv4Packet::owned(ipbuf).unwrap();
+        ip_header.set_header_length(69);
+        ip_header.set_total_length(52);
+        ip_header.set_fragment_offset(16384);
+        ip_header.set_next_level_protocol(IpNextHeaderProtocols::Tcp);
+        ip_header.set_source(src_ip);
+        ip_header.set_destination(dst_ip);
+        ip_header.set_identification(rand::random::<u16>());
+        ip_header.set_ttl(128);
+        ip_header.set_version(4);
+        ip_header.set_payload(&tcp_header.packet());
+
+        let checksum = pnet::packet::ipv4::checksum(&ip_header.to_immutable());
+        ip_header.set_checksum(checksum);
+
+        ip_header
+    }
 }
 
-#[cfg(test)]
-pub fn build_dummy_ip(
-    src_ip: Ipv4Addr,
-    dst_ip: Ipv4Addr,
-    src_port: u16,
-    dst_port: u16,
-) -> MutableIpv4Packet<'static> {
-    // Setup TCP header
-    let mut vec: Vec<u8> = vec![0; TCP_HEADER_LEN];
-    let mut tcp_header = MutableTcpPacket::new(&mut vec[..]).unwrap();
 
-    tcp_header.set_source(src_port);
-    tcp_header.set_destination(dst_port);
 
-    tcp_header.set_flags(tcp::TcpFlags::SYN);
-    tcp_header.set_window(64240);
-    tcp_header.set_data_offset(8);
-    tcp_header.set_urgent_ptr(0);
-    tcp_header.set_sequence(rand::random::<u32>());
 
-    let checksum = pnet::packet::tcp::ipv4_checksum(&tcp_header.to_immutable(), &src_ip, &dst_ip);
-    tcp_header.set_checksum(checksum);
-
-    // Setup IP header
-    let ipbuf: Vec<u8> = vec![0; TCP_HEADER_LEN + IPV4_HEADER_LEN];
-    let mut ip_header = MutableIpv4Packet::owned(ipbuf).unwrap();
-    ip_header.set_header_length(69);
-    ip_header.set_total_length(52);
-    ip_header.set_fragment_offset(16384);
-    ip_header.set_next_level_protocol(IpNextHeaderProtocols::Tcp);
-    ip_header.set_source(src_ip);
-    ip_header.set_destination(dst_ip);
-    ip_header.set_identification(rand::random::<u16>());
-    ip_header.set_ttl(128);
-    ip_header.set_version(4);
-    ip_header.set_payload(&tcp_header.packet());
-
-    let checksum = pnet::packet::ipv4::checksum(&ip_header.to_immutable());
-    ip_header.set_checksum(checksum);
-
-    ip_header
-}
 
 // only use for health checks
 pub fn allocate_socket(listen_ip: Ipv4Addr) -> Option<Socket> {
